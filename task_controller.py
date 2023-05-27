bbca50256fdd0678e48e76df47390688865b6855
bradgalben <77229663+bradgalben@users.noreply.github.com>
2023-05-27 13:42:18 +0200

    Update task_controller.py

--- task_controller.py
+++ task_controller.py
@@ -1,32 +1,23 @@
 
-import numpy as np
-import pandas as pd
+from numpy import array
+#from pandas import DataFrame, unique
 
 class JOB_Control:
     
-    def __init__(self, df):
+    def __init__(self):
         
-        assert list(df.columns) == ['Job_ID', 'Task_ID', 
-                                    'Arrival_Time', 'CPU', 'Memory'], 'Incorrect input format'
-        
-        arrival_times = np.array(pd.unique(df['Arrival_Time']))
-        id_s = np.array(pd.unique(df['Job_ID']))
-        count = np.array(df.groupby('Job_ID')["Task_ID"].count())
-        tot_cpu = np.array(df.groupby('Job_ID')["CPU"].sum())*10000
-        
-        self.register = {str(a_id):{'Active_tasks':k, 'Arrival': t, 'Completion': 0} 
-                                       for (a_id, k, t, num) in zip(id_s, count, arrival_times,tot_cpu)}
+        self.register = [] # list of dict
     
-    def preprocess(task, mu=0.1):
+    def preprocess(self, task : dict, mu=0.1):
     
         assert list(task.keys()) == ['Job_ID', 'Task_ID', 
                                     'Arrival_Time', 'CPU', 'Memory'], 'Incorrect input format'
         
         new_job_id = str(int(task["Job_ID"]))+ "_" + str(int(task["Task_ID"]))
-        arrival_time_task = int(task["Arrival_Time"])
-        running_time_task = (task["CPU"]/mu)*1000
+        arrival_time_task = task["Arrival_Time"]
+        running_time_task = (task["CPU"]/mu)
 
         a_dict = {"id": new_job_id, "Memory": task['Memory'], 
-                "arrival_time": arrival_time_task, "service_time" : running_time_task}
+                "Arrival_Time": arrival_time_task, "service_time" : running_time_task}
         
         return a_dict
