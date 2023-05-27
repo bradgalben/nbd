5d244aedc989d5abad357404affde0ba3977f135
bradgalben <77229663+bradgalben@users.noreply.github.com>
2023-05-27 13:38:17 +0200

    Update Scheduling.py

--- FCFS.py
+++ FCFS.py
@@ -1,28 +1,29 @@
 
-class FirstComeFirstService:
+
+class  First_Come_First_Service:
     
-    def __init__(self, id):
+    def __init__(self, id : int):
+
         self.server_id = id 
         self.status = 0
-        self.memory = 1 
+        self.memory = 1
+        self.work_load = 0
         self.queue = []
         self.details = []
-        self.work_load = 0
-
-        
+    
     def add_to_queue(self, task : dict):
         
         task['waiting_time'] = 0
 
-        if self.memory > task["Memory"]: 
+        #if self.memory > task["Memory"]: 
             
-            self.queue.append(task)   
-            self.status = 1
-            self.memory -= task["Memory"]
-            self.work_load = sum([task['service_time'] if self.status == 1 else 0 for task in self.queue])
-            self.details.append(task['service_time'])
+        self.queue.append(task)   
+        self.status = 1
+        self.memory -= task["Memory"]
+        self.work_load = sum([task['service_time'] if self.status == 1 else 0 for task in self.queue])
+        self.details.append(task['service_time'])
                 
-        else: return False
+        #else: return False
             #raise Exception('Memory error!')
             
     def delete_from_queue(self):
@@ -31,4 +32,3 @@ class FirstComeFirstService:
         self.queue.pop(0)
         if len(self.queue) == 0: self.status = 0
         self.work_load = sum([task['service_time'] if self.status == 1 else 0 for task in self.queue])
-   
